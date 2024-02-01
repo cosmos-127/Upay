9791aef0f9207c0430b92c2de9eadab777818776
cosmos-127 <cosmos.dev.127@gmail.com>
2024-02-02 02:45:14 +0530

    nothing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,14 @@ import Signup from "./screens/Signup";
 import Signin from "./screens/Signin";
 import Dashboard from "./screens/Dashboard";
 import { SendMoney } from "./screens/SendMoney";
-
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 function App() {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		navigate("/signin"); 
+	}, [navigate]);
 	return (
 		<>
 			<BrowserRouter>
